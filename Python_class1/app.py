a2da58467bfc3904069e46e3d92a40ff7a19ed8b
Edoghotu Gideon Azibaobuom <gideon.edoghotu@cyphercrescent.com>
2023-10-19 17:19:03 +0100

    updated

--- Python_class1/app.py
+++ Python_class1/app.py
@@ -15,9 +15,12 @@ i = 0
 while (i < 3):
     guess = int(input("Guess: "))
 
-    if guess != 9 and :
+    if guess != 9 and i != 2:
+        i += 1
         continue
     elif (guess == 9):
         print("you got it")
+    else:
+        print("sorry you failed")
 
 
