c6d83c5db6b5aad9ac51b8f8616bf761565da03b
Edoghotu Gideon Azibaobuom <gideon.edoghotu@cyphercrescent.com>
2023-10-19 17:14:13 +0100

    updated

---
+++ Python_class1/app.py
@@ -0,0 +1,23 @@
+
+"""
+course = '''
+Hi Gideon,
+
+Here is our first email to you.
+
+Thank you.
+
+'''
+
+"""
+
+i = 0
+while (i < 3):
+    guess = int(input("Guess: "))
+
+    if guess != 9 and :
+        continue
+    elif (guess == 9):
+        print("you got it")
+
+
